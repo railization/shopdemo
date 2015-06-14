4dd4e723b090cd650c2177eadeb47d43ce4a25a1
railization <mab@railization.com>
2015-06-14 17:35:02 +0200

    add validations for users and listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -18,4 +18,8 @@ else
     :dropbox_credentials => Rails.root.join("/config/dropbox.yml"),
     :path => ":style/:id_:filename"
     end  
+
+    validates :name, :description, :price, presence:true
+    validates :price, numericality:{greater_than: 0}
+	validates_attachment_presence :image
 end
