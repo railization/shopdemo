d91193e9bd3b6625d94f8bb7d561f5d16acb5f49
railization <mab@railization.com>
2015-06-14 19:24:02 +0200

    associate listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -22,4 +22,6 @@ else
     validates :name, :description, :price, presence:true
     validates :price, numericality:{greater_than: 0}
 	validates_attachment_presence :image
+
+	belongs_to :user
 end
